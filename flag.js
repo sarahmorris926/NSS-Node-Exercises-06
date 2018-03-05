059efcd9163faaf080e026a4239b2783db1c3eff
Sarah Morris <sarah.morris9@gmail.com>
2018-03-05 15:54:17 -0600

    Completed exercise

--- flag.js
+++ flag.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
 
+const {bgblue, bgRed, white, whiteBright} = require('chalk');
+const star = '\u272f'
+
+
+console.log(white.bgBlue.bold(` ${star} ${star} ${star} ${star} ${star} ${star} ${star} `), bgRed.bold('                                   '));
+console.log(white.bgBlue.bold(`  ${star} ${star} ${star} ${star} ${star} ${star}  `), whiteBright.bold('                                   '));
+console.log(white.bgBlue.bold(` ${star} ${star} ${star} ${star} ${star} ${star} ${star} `), bgRed.bold('                                   '));
+console.log(white.bgBlue.bold(`  ${star} ${star} ${star} ${star} ${star} ${star}  `), whiteBright.bold('                                   '));
+console.log(white.bgBlue.bold(` ${star} ${star} ${star} ${star} ${star} ${star} ${star} `), bgRed.bold('                                   '));
+console.log(white.bgBlue.bold(`  ${star} ${star} ${star} ${star} ${star} ${star}  `), whiteBright.bold('                                   '));
+console.log(white.bgBlue.bold(` ${star} ${star} ${star} ${star} ${star} ${star} ${star} `), bgRed.bold('                                   '));
+console.log(whiteBright.bold('                                                   '));
+console.log(bgRed.bold('                                                   '));
+console.log(whiteBright.bold('                                                   '));
+console.log(bgRed.bold('                                                   '));
+console.log(whiteBright.bold('                                                   '));
+console.log(bgRed.bold('                                                   '));
+
+
